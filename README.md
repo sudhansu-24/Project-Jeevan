4e52e862634d347128da07ec7691dcef070cdb80
Sudhansu-24 <ssprusty98@gmail.com>
2024-07-27 17:09:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+  
+<br />
+
+  <h2 align="center">Project Jeevan</h2>
+
+  Jeevan is a fully responsive hospital website, <br />Responsive for all devices, build using HTML, CSS, and JavaScript.
+
+  <a href="https://jeevantest2l.netlify.app/"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+
+
+### Contact
+
+If you want to contact with me you can reach me at [Twitter](https://www.twitter.com/Sudhansu_24).
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * PRELOADER
+ * 
+ * preloader will be visible until document load
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * MOBILE NAVBAR
+ * 
+ * show the mobile navbar when click menu button
+ * and hidden after click menu close button or overlay
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
